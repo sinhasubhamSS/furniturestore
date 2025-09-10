e9d73b43e899b52022ef01256edaa216f37b7e88
sinhasubhamSS <subhamsinhass.344@gmail.com>
2025-09-10 09:46:14 +0530

    url changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 const app = express();
 app.use(
   cors({
-  origin: ["https://yourfrontend.onrender.com", "http://localhost:3000"],
+  origin: ["https://suvidhafurniture.vercel.app", "http://localhost:3000"],
     credentials: true, // 👈 important if using cookies
   })
 );
